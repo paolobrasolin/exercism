304090811d7df3f2e7745e509e66e232015556f8
Paolo Brasolin <paolo.brasolin@gmail.com>
2019-03-05 22:16:14 +0100

    apply feedback to haskell/leap

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,5 +1,5 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = divBy 4 && (divBy 400 || (not.divBy) 100)
-  where divBy n = 0 == mod year n
+isLeapYear year = divBy 4 && ((not.divBy) 100 || divBy 400)
+  where divBy n = 0 == rem year n

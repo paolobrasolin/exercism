a6d067d0dbe5d34826315656e816cc85e5fd7c39
Paolo Brasolin <paolo.brasolin@gmail.com>
2019-03-13 00:22:59 +0100

    apply feedback to ruby/hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,6 +1,6 @@
 module Hamming
   def self.compute(dna, dnb)
     raise ArgumentError if dna.size != dnb.size
-    0.upto(dna.size).sum { |i| dna[i] != dnb[i] ? 1 : 0 }
+    0.upto(dna.size).count { |i| dna[i] != dnb[i] }
   end
 end

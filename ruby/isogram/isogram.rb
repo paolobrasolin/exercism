0343306aecdcd4ff565b69ede310a8630a0667ca
Paolo Brasolin <paolo.brasolin@gmail.com>
2019-03-14 00:38:39 +0100

    apply feedback to ruby/isogram

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,6 +1,8 @@
 module Isogram
   def self.isogram?(word)
-    letters = word.downcase.scan(/[a-z]/)
-    letters.uniq.size == letters.size
+    not word.each_char.with_object(Hash.new(false)) do |char, toggler|
+      next unless char =~ /[a-z]/i
+      break unless toggler[char.downcase] ^= true
+    end.nil?
   end
 end

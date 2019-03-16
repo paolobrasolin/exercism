041dbf66a0cf61b05cb7f67b435d22cdbc24a37b
Paolo Brasolin <paolo.brasolin@gmail.com>
2019-03-16 22:39:33 +0100

    apply feedback to haskell/bob

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Bob (responseFor) where
 
 import Data.Char (isSpace, isLetter, isUpper)
@@ -5,12 +7,12 @@ import qualified Data.Text as T
 
 responseFor :: T.Text -> T.Text
 responseFor xs
-  | isSilence = T.pack "Fine. Be that way!"
+  | isSilence = "Fine. Be that way!"
   | isYelling &&
-    isInquiry = T.pack "Calm down, I know what I'm doing!"
-  | isYelling = T.pack "Whoa, chill out!"
-  | isInquiry = T.pack "Sure."
-  | otherwise = T.pack "Whatever."
+    isInquiry = "Calm down, I know what I'm doing!"
+  | isYelling = "Whoa, chill out!"
+  | isInquiry = "Sure."
+  | otherwise = "Whatever."
   where
     compact = T.filter (not.isSpace) xs
     letters = T.filter isLetter compact

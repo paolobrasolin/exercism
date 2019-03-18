8dc06e48b9e89417d3910ea359864f96b48bbd60
Paolo Brasolin <paolo.brasolin@gmail.com>
2019-03-18 23:59:50 +0100

    apply feedback to haskell/collatz-conjecture

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,17 +1,14 @@
 module CollatzConjecture (collatz) where
 
+import Data.List (elemIndex)
+
 collatz :: Integer -> Maybe Integer
 collatz n
   | n <= 0 = Nothing
-  | otherwise = Just $ succToFix 0 collatzOp n
+  | otherwise = fmap toInteger $ elemIndex 1 $ iterate collatzOp n
 
 collatzOp :: (Integral a) => a -> a
 collatzOp n
   | 1 == n = 1
   | odd n = 3 * n + 1
-  | otherwise = div n 2
-
-succToFix :: (Enum i, Eq b) => i -> (b -> b) -> b -> i
-succToFix i f x
-  | f x == x = i
-  | otherwise = succToFix (succ i) f (f x)
+  | otherwise = quot n 2

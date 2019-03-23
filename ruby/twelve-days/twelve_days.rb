f9f60b44ed83e3a4c2456ace61cc4ad6589ad370
Paolo Brasolin <paolo.brasolin@gmail.com>
2019-03-23 16:34:52 +0100

    apply feedback to ruby/twelve-days

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -3,6 +3,7 @@ module TwelveDays
     "first", "second", "third", "fourth", "fifth", "sixth",
     "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth",
   ]
+  private_constant :ORDINALS
 
   GIFTS = [
     "a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens",
@@ -10,18 +11,30 @@ module TwelveDays
     "seven Swans-a-Swimming", "eight Maids-a-Milking", "nine Ladies Dancing",
     "ten Lords-a-Leaping", "eleven Pipers Piping", "twelve Drummers Drumming",
   ]
+  private_constant :GIFTS
 
   def self.song
-    1.upto(12).map(&method(:day)).join("\n")
+    1.upto(12).map(&method(:verse)).join("\n")
   end
 
-  def self.day(n)
-    gift_list = to_sentence(*GIFTS[0..n-1].reverse)
-    "On the #{ORDINALS[n-1]} day of Christmas my true love gave to me: #{gift_list}.\n"
-  end
+  class << self
+  private
 
-  def self.to_sentence(*items)
-    *init, last = items
-    init.empty? ? last : "#{init.join(", ")}, and #{last}"
+    def verse(n)
+      "On the #{human_ordinal(n)} verse of Christmas my true love gave to me: #{human_gifts_list(n)}.\n"
+    end
+
+    def human_ordinal(n)
+      ORDINALS[n-1]
+    end
+
+    def human_gifts_list(n)
+      to_sentence(*GIFTS[0..n-1].reverse)
+    end
+
+    def to_sentence(*items)
+      *init, last = items
+      init.empty? ? last : "#{init.join(", ")}, and #{last}"
+    end
   end
 end

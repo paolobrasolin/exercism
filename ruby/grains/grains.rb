33c5e5d144a0b27cb879884d308b9deec3c07aba
Paolo Brasolin <paolo.brasolin@gmail.com>
2019-03-24 14:24:58 +0100

    solve ruby/grains

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,10 +1,12 @@
 module Grains
+  BOARD_SIZE = 64
+
   def self.square(n)
-    raise ArgumentError unless (1..64).include?(n)
+    raise ArgumentError unless (1..BOARD_SIZE).include?(n)
     2**(n-1)
   end
 
   def self.total
-    2**64-1
+    2**BOARD_SIZE-1
   end
 end

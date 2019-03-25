c482e89adcd83a4732f66b5a93612330c0ca620e
Paolo Brasolin <paolo.brasolin@gmail.com>
2019-03-25 20:12:12 +0100

    apply feedback to haskell/rna-transcription

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,10 +1,8 @@
 module DNA (toRNA) where
 
-import Control.Applicative (liftA2)
-
 toRNA :: String -> Either Char String
 toRNA "" = Right ""
-toRNA (d:ds) = liftA2 (:) (nucleotideToRNA d) (toRNA ds)
+toRNA (d:ds) = (:) <$> (nucleotideToRNA d) <*> (toRNA ds)
 
 nucleotideToRNA :: Char -> Either Char Char
 nucleotideToRNA d = case d of

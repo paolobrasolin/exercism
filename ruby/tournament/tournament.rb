3ca41edb3314ac891bdcc7e39ccb9405ac189810
Paolo Brasolin <paolo.brasolin@gmail.com>
2019-04-24 20:20:53 +0200

    apply feedback to ruby/tournament

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,33 +1,37 @@
-class Team
-  attr_reader :name, :win, :draw, :loss
+class Result < Array
+  FLIPPED = %w{win loss draw draw loss win}.each_slice(2).to_h
+  SCORES = { "win" => 3, "draw" => 1, "loss" => 0 }
 
-  def initialize(name)
-    @name = name
-    @win = 0
-    @draw = 0
-    @loss = 0
-  end
+  attr_reader :team_name
 
-  def play(outcome)
-    case outcome
-    when "win"  then @win  += 1
-    when "draw" then @draw += 1
-    when "loss" then @loss += 1
-    end
-  end
-
-  def matches_played
-    [@win, @draw, @loss].sum
+  def initialize(team_name)
+    @team_name = team_name
   end
 
   def points
-    [@win * 3, @draw * 1, @loss * 0].sum
+    sum(&SCORES)
+  end
+
+  def <=>(result)
+    [-points, team_name] <=> [-result.points, result.team_name]
+  end
+
+  def to_board_row
+    [
+      team_name,
+      size, # matches played
+      count("win"),
+      count("draw"),
+      count("loss"),
+      points,
+    ]
   end
 end
 
 class Board
   HEADER = %w{Team MP W D L P}
   SPACER = " | "
+  NEWLINE = "\n"
   NAME_COL_WIDTH = 30
   DATA_COL_WIDTH = 2
 
@@ -40,7 +44,7 @@ class Board
   end
 
   def render
-    @rows.map { |row| render_row(*row) + "\n" }.join
+    @rows.map { |row| render_row(*row) + NEWLINE }.join
   end
 
 private
@@ -55,45 +59,21 @@ end
 
 module Tournament
   DATAROW = /^(.*?);(.*?);(.*?)$/
-  FLIP_OUTCOME = %w{win draw loss}.
-    yield_self { |os| os.reverse.zip(os).to_h }
 
   def self.tally(input)
-    teams = read_teams(input)
-    sorted_teams = sort_teams(teams)
-
-    board_team_rows = sorted_teams.map(&method(:team_to_board_row))
-
-    board = Board.new
-    board.concat(board_team_rows)
-    board.render
+    board_rows = read_results(input).sort.map(&:to_board_row)
+    Board.new.tap { |board| board.concat(board_rows) }.render
   end
 
   class << self
   private
 
-    def read_teams(input)
-      teams = Hash.new { |h, name| h[name] = Team.new(name) }
-      input.scan(DATAROW).each do |team_a, team_b, outcome|
-        teams[team_a].play(outcome)
-        teams[team_b].play(FLIP_OUTCOME[outcome])
-      end
-      teams.values
-    end
-
-    def sort_teams(teams)
-      teams.sort_by { |team| [-team.points, team.name] }
-    end
-
-    def team_to_board_row(team)
-      [
-        team.name,
-        team.matches_played,
-        team.win,
-        team.draw, 
-        team.loss,
-        team.points,
-      ]
+    def read_results(input)
+      roster = Hash.new { |h, k| h[k] = Result.new(k) }
+      input.scan(DATAROW).each_with_object(roster) do |(home, away, result), roster|
+        roster[home] << result
+        roster[away] << Result::FLIPPED[result]
+      end.values
     end
   end
 end
